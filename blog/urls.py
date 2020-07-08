dddac63d802861a3d09087813c6ee68f1f9c26c0
Max Garin <maxgarin@gmail.com>
2020-07-08 16:28:50 -0400

    Fix layout add menu for recent blog post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PostList
+from .views import PostList, PostDetail
 app_name = 'blog'
 urlpatterns = [
     path('list/', PostList.as_view(), name='list'),
+    path('post/<int:pk>', PostDetail.as_view(), name='detail')
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from pprint import pprint
+from blog.models import Post
 
 
 def index(request):
-
-    return render(request, 'pages/index.html')
+    recent_post = Post.objects.order_by('-created_on')[:3]
+    return render(request, 'pages/index.html', context={"recent_post": recent_post})
